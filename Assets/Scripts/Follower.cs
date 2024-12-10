9bdabb943dcd759fa2fded0ffa09e5ef76af8d94
yehonatna <yonidil1997@gmail.com>
2024-12-10 14:52:53 +0200

    fix

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Follower : MonoBehaviour
 {
     [SerializeField, Tooltip("Add the objecy that you want this object will follow")] GameObject objectToFollow;
-    
+
     void Update()
     {
         if (objectToFollow != null)
