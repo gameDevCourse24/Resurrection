07822ef94cf0efe8f2cc4c30eb957e510d841e5e
yehonatna <yonidil1997@gmail.com>
2024-12-10 14:46:44 +0200

    start with unity

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField, Tooltip("Add the objecy that you want this object will follow")] GameObject objectToFollow;
+    
+    void Update()
+    {
+        if (objectToFollow != null)
+        {
+            transform.position = objectToFollow.transform.position;
+        }
+    }
+}
